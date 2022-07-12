694a0d4ff917156cd52dd7fa32a3670ccb4129d6
Arthur <arthur.olkov@gmail.com>
2022-07-13 00:00:39 +0500

    Seminar2_HomeWork add Task 1

---
+++ Seminar2_HomeWork/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 1: Программа, которая принимает на вход трёхзначное число и 
+//           на выходе показывает вторую цифру этого числа.
+
+int SecondDigit(int num)
+{
+    return num / 10 % 10;
+}
+
+Console.Write("Inter number: ");
+int digit = SecondDigit (Convert.ToInt32(Console.ReadLine()));
+Console.WriteLine("Second digit is: " + digit);
